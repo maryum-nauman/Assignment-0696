56b51f0b8589d5ca115cc93f670e6194b6c7ca90
maryum-nauman <l230696@lhr.nu.edu.pk>
2026-03-26 16:28:08 +0500

    Added menu
    Added menu on home fragment to view last successful booking

--- app/src/main/java/com/example/assignment_0696/HomeFragment.java
+++ app/src/main/java/com/example/assignment_0696/HomeFragment.java
@@ -2,30 +2,21 @@ package com.example.assignment_0696;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.content.res.ColorStateList;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.net.Uri;
-import android.content.Intent;
+import android.view.*;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
-import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Lifecycle;
+
 import androidx.viewpager2.widget.ViewPager2;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
-
-import org.jspecify.annotations.NonNull;
 
 public class HomeFragment extends Fragment {
 
@@ -35,12 +26,11 @@ public class HomeFragment extends Fragment {
     public HomeFragment() {
         super(R.layout.fragment_home);
     }
-
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+
         tabLayout = view.findViewById(R.id.tabLayout);
         viewPager = view.findViewById(R.id.viewPager);
-
         ViewPagerAdapter adapter = new ViewPagerAdapter(this);
         viewPager.setAdapter(adapter);
 
@@ -50,34 +40,38 @@ public class HomeFragment extends Fragment {
                     else tab.setText("Coming Soon");
                 }).attach();
 
-        setHasOptionsMenu(true);
-    }
+        ImageButton btnMenu = view.findViewById(R.id.btnMenu);
+        btnMenu.setOnClickListener(v -> {
+            PopupMenu popupMenu = new PopupMenu(requireContext(), btnMenu);
+            popupMenu.inflate(R.menu.menu_home);
+            popupMenu.setOnMenuItemClickListener(item -> {
+                if (item.getItemId() == R.id.viewBooking) {
+                    SharedPreferences prefs =
+                            requireContext().getSharedPreferences("BookingPrefs", Context.MODE_PRIVATE);
+                    String movie = prefs.getString("movie", null);
+                    if (movie == null) {
+                        Toast.makeText(getContext(),
+                                "No previous booking found",
+                                Toast.LENGTH_SHORT).show();
+                    } else {
+                        int seats = prefs.getInt("seats", 0);
+                        float total = prefs.getFloat("total", 0);
 
-    @Override
-    public void onCreateOptionsMenu(@NonNull Menu menu, MenuInflater inflater) {
-        inflater.inflate(R.menu.menu_home, menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.viewBooking) {
-            SharedPreferences prefs = requireContext().getSharedPreferences("booking", Context.MODE_PRIVATE);
-
-            String movie = prefs.getString("movie", null);
-
-            if (movie == null) {
-                Toast.makeText(getContext(), "No previous booking found", Toast.LENGTH_SHORT).show();
-            } else {
-                int seats = prefs.getInt("seats", 0);
-                int price = prefs.getInt("price", 0);
-
-                new AlertDialog.Builder(getContext())
-                        .setTitle("Last Booking")
-                        .setMessage("Movie: " + movie + "\nSeats: " + seats + "\nTotal Price: $" + price)
-                        .setPositiveButton("OK", null)
-                        .show();
-            }
-        }
-        return true;
+                        new AlertDialog.Builder(requireContext())
+                                .setTitle("Last Booking")
+                                .setMessage(
+                                        "Movie: " + movie +
+                                                "\nSeats: " + seats +
+                                                "\nTotal Price: $" + total
+                                )
+                                .setPositiveButton("OK", null)
+                                .show();
+                    }
+                    return true;
+                }
+                return false;
+            });
+            popupMenu.show();
+        });
     }
 }
